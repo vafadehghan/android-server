4568cc245d0109af7b6df299792099f175c9e5d7
Vafa Dehghan Saei <v.dehghansaei@gmail.com>
2018-04-05 18:27:21 -0700

    refactor

--- public/map.js
+++ public/map.js
@@ -1,10 +1,58 @@
+/*---------------------------------------------------------------------------------------
+--	SOURCE FILE: map.js - The Google Maps API caller
+--
+--	PROGRAM:     Location Finder
+--
+--	DATE:        April 5, 2018
+--
+--	FUNCTIONS:
+--					int main (int argc, char **argv)
+--					void* readThreadFunc()
+--					void* sendThreadFunc()
+--					void printFunc()
+--					void signal_catcher(int signo)
+--
+--
+--	DESIGNERS:		Vafa Dehghan Saei
+--
+--	PROGRAMMERS:	Vafa Dehghan Saei
+--
+--
+--	NOTES:
+--	The program will establish a TCP connection to a user specifed server.
+--  The server can be specified using a fully qualified domain name or and
+--	IP address. After the connection has been established the user can type
+--  messages to be sent to other connected clients. The client will also
+--  display any incoming message sent from other clients through the server.
+---------------------------------------------------------------------------------------*/
+
 var x = 0;
 var y = 0;
-var name;
-
+var userName;
 var markers = [];
 var map;
 
+/*----------------------------------------------------------------------
+-- FUNCTION:	  newConnection
+--
+-- DATE:        April 5, 2018
+--
+-- DESIGNER:    Vafa Dehghan Saei
+--
+-- PROGRAMMER:  Vafa Dehghan Saei
+--
+-- INTERFACE: 	function newConnection(connection)
+--
+-- ARGUMENT:    var connection - an object representing a connected client
+--
+--
+-- RETURNS:     void
+--
+-- NOTES:
+-- When there is a new connection this function will run.
+-- The clients address is printed to the console.
+-- This function will also define the functions to handle the new data, disconnect and error events.
+----------------------------------------------------------------------*/
 function clearOverlays() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
@@ -12,6 +60,27 @@ function clearOverlays() {
   markers.length = 0;
 }
 
+/*----------------------------------------------------------------------
+-- FUNCTION:	  newConnection
+--
+-- DATE:        April 5, 2018
+--
+-- DESIGNER:    Vafa Dehghan Saei
+--
+-- PROGRAMMER:  Vafa Dehghan Saei
+--
+-- INTERFACE: 	function newConnection(connection)
+--
+-- ARGUMENT:    var connection - an object representing a connected client
+--
+--
+-- RETURNS:     void
+--
+-- NOTES:
+-- When there is a new connection this function will run.
+-- The clients address is printed to the console.
+-- This function will also define the functions to handle the new data, disconnect and error events.
+----------------------------------------------------------------------*/
 function initMap() {
 
   var mapOptions = {
@@ -145,7 +214,27 @@ function initMap() {
 }
 
 setInterval(loop, 1000);
-
+/*----------------------------------------------------------------------
+-- FUNCTION:	  newConnection
+--
+-- DATE:        April 5, 2018
+--
+-- DESIGNER:    Vafa Dehghan Saei
+--
+-- PROGRAMMER:  Vafa Dehghan Saei
+--
+-- INTERFACE: 	function newConnection(connection)
+--
+-- ARGUMENT:    var connection - an object representing a connected client
+--
+--
+-- RETURNS:     void
+--
+-- NOTES:
+-- When there is a new connection this function will run.
+-- The clients address is printed to the console.
+-- This function will also define the functions to handle the new data, disconnect and error events.
+----------------------------------------------------------------------*/
 function loop() {
 
   var Httpreq = new XMLHttpRequest();
@@ -155,7 +244,8 @@ function loop() {
 
   json = JSON.parse(Httpreq.responseText);
 
-  deleteMarkers();
+  setMapOnAll(null);
+  markers = [];
 
   for (i in json) {
     for (j in json[i]) {
@@ -164,9 +254,9 @@ function loop() {
 
       x = t.x;
       y = t.y;
-      name = t.name;
-      var title = '' + t.ip.substring(7) +' ' + name;
-      console.log("name: " + name + " (" + x + "," + y + ")");
+      userName = t.name;
+      var title = '' + t.ip.substring(7) + ' ' + userName;
+      console.log("name: " + userName + " (" + x + "," + y + ")");
 
       var marker = new google.maps.Marker({
         position: {
@@ -197,32 +287,32 @@ function loop() {
   }
 
   setMapOnAll(map);
-  showMarkers();
-
 }
 
-
-// GMAPS TOOLS
-
+/*----------------------------------------------------------------------
+-- FUNCTION:	  newConnection
+--
+-- DATE:        April 5, 2018
+--
+-- DESIGNER:    Vafa Dehghan Saei
+--
+-- PROGRAMMER:  Vafa Dehghan Saei
+--
+-- INTERFACE: 	function newConnection(connection)
+--
+-- ARGUMENT:    var connection - an object representing a connected client
+--
+--
+-- RETURNS:     void
+--
+-- NOTES:
+-- When there is a new connection this function will run.
+-- The clients address is printed to the console.
+-- This function will also define the functions to handle the new data, disconnect and error events.
+----------------------------------------------------------------------*/
 // Sets the map on all markers in the array.
 function setMapOnAll(map) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
   }
-}
-
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-  setMapOnAll(null);
-}
-
-// Shows any markers currently in the array.
-function showMarkers() {
-  setMapOnAll(map);
-}
-
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-  clearMarkers();
-  markers = [];
 }
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+/*---------------------------------------------------------------------------------------
+--	SOURCE FILE: server.js - The web server for the location finder
+--
+--	PROGRAM:     Location Finder
+--
+--	DATE:        April 5, 2018
+--
+--	FUNCTIONS:
+--					function newConnection(connection)
+--					function clientConnected(d)
+--          function clientDisconnect()
+--					function error(err)
+--
+--
+--	DESIGNERS:		Vafa Dehghan Saei
+--
+--	PROGRAMMERS:	Vafa Dehghan Saei
+--
+--
+--	NOTES:
+--  This program will create a server that will listen on port 9000.
+--  The program will make an entry into the client dictionary with the location and name of the client.
+--  It will then add the location and name to the JSON file which will be read by the map.
+--  Once the client is disconnected the entry is removed from the JSON.
+---------------------------------------------------------------------------------------*/
+
 var net = require('net');
 var express = require('express');
 var path = require('path');
 var app = express();
-
 var clients = {};
 var now = new Date();
-
 var client = function(name, x, y, ip) {
   return {
     "name": name,
@@ -14,16 +38,35 @@ var client = function(name, x, y, ip) {
     "ip": ip
   };
 };
-
-
 var server = net.createServer();
+
 server.on('connection', newConnection);
 
 server.listen(9000, function() {
   console.log('server listening to: ' + JSON.stringify(server.address()) + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
 });
 
-
+/*----------------------------------------------------------------------
+-- FUNCTION:	  newConnection
+--
+-- DATE:        April 5, 2018
+--
+-- DESIGNER:    Vafa Dehghan Saei
+--
+-- PROGRAMMER:  Vafa Dehghan Saei
+--
+-- INTERFACE: 	function newConnection(connection)
+--
+-- ARGUMENT:    var connection - an object representing a connected client
+--
+--
+-- RETURNS:     void
+--
+-- NOTES:
+-- When there is a new connection this function will run.
+-- The clients address is printed to the console.
+-- This function will also define the functions to handle the new data, disconnect and error events.
+----------------------------------------------------------------------*/
 function newConnection(connection) {
   var remoteAddress = connection.remoteAddress + ':' + connection.remotePort;
   console.log('new client connection from: ' + remoteAddress + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
@@ -34,6 +77,28 @@ function newConnection(connection) {
 
   clients[connection.remoteAddress] = client("name", 0, 0, connection.remoteAddress);
 
+
+  /*----------------------------------------------------------------------
+  -- FUNCTION:	  clientConnected
+  --
+  -- DATE:        April 5, 2018
+  --
+  -- DESIGNER:    Vafa Dehghan Saei
+  --
+  -- PROGRAMMER:  Vafa Dehghan Saei
+  --
+  -- INTERFACE: 	function clientConnected(d)
+  --
+  -- ARGUMENT:    var d - a Buffer or String which is the data received
+  --
+  --
+  -- RETURNS:     void
+  --
+  -- NOTES:
+  -- When there is a new connection this function will run.
+  -- The clients name and location is added to a client dictionary.
+  -- This function will also define the functions to handle the new data, disconnect and error events.
+  ----------------------------------------------------------------------*/
   function clientConnected(d) {
     console.log('Address: ' + remoteAddress + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
     console.log('Data: ' + d + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
@@ -51,9 +116,28 @@ function newConnection(connection) {
     clients[connection.remoteAddress].x = client_x;
     clients[connection.remoteAddress].y = client_y;
 
-    connection.write(d);
   }
 
+  /*----------------------------------------------------------------------
+  -- FUNCTION:	  clientDisconnect
+  --
+  -- DATE:        April 5, 2018
+  --
+  -- DESIGNER:    Vafa Dehghan Saei
+  --
+  -- PROGRAMMER:  Vafa Dehghan Saei
+  --
+  -- INTERFACE: 	function clientDisconnect()
+  --
+  -- ARGUMENT:    void
+  --
+  --
+  -- RETURNS:     void
+  --
+  -- NOTES:
+  -- When a client disconnects this function is called.
+  -- The client is removed from the dictionary, which will also remove it from the JSON.
+  ----------------------------------------------------------------------*/
   function clientDisconnect() {
     delete clients[connection.remoteAddress];
     console.log('connection closed: ' + remoteAddress + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
@@ -61,6 +145,26 @@ function newConnection(connection) {
 
   }
 
+  /*----------------------------------------------------------------------
+  -- FUNCTION:	  error
+  --
+  -- DATE:        April 5, 2018
+  --
+  -- DESIGNER:    Vafa Dehghan Saei
+  --
+  -- PROGRAMMER:  Vafa Dehghan Saei
+  --
+  -- INTERFACE: 	function error(err)
+  --
+  -- ARGUMENT:    var err - an object representing the error
+  --
+  --
+  -- RETURNS:     void
+  --
+  -- NOTES:
+  -- When there is an error this function will run.
+  -- The error is printed to the console.
+  ----------------------------------------------------------------------*/
   function error(err) {
     console.log('err' + remoteAddress + " " + err.message + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
   }
@@ -68,18 +172,15 @@ function newConnection(connection) {
 }
 
 app.set('views', __dirname + '/views');
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get('/data', function(req, res) {
-
-
   var password = req.query.password;
-
   if (password == "dc4985") {
     res.json({
       clients
@@ -87,7 +188,7 @@ app.get('/data', function(req, res) {
   } else {
     return res.status(403).send({
       success: false,
-      message: 'Invalid password'
+      message: 'Incorrect Password'
     });
   }
 });
